1277df4e8185e71bdee9bf8498c82958d34ec537
Rahul Rathor <rathore.vaishali1011@gmail.com>
2025-09-26 07:17:55 +0530

    update

--- src/app/(auth)/signup/action.ts
+++ src/app/(auth)/signup/action.ts
@@ -9,8 +9,8 @@ export async function signupAction(formData: SignupCombineData) {
   }
 
   try {
-      console.log('process.env.NEXT_PUBLIC_BACKEND_URL:', process.env.NEXT_PUBLIC_BACKEND_URL+'/auth/signup')
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/signup`, {
+      console.log('process.env.NEXT_PUBLIC_BACKEND_URL:', process.env.NEXT_PUBLIC_PROD_BACKEND_URL+'/auth/signup')
+    const res = await fetch(`${process.env.NEXT_PUBLIC_PROD_BACKEND_URL}/auth/signup`, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(parsed.data), // ✅ rememberMe not included here
